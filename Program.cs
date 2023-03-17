cde611ef83ac6ae226f2345edc5a852ec2a8e2b7
hp <hp@HPELITEBOOK840G>
2023-03-17 23:33:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+public class Factors
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter Any Number :");
+        int Number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Fectors Number of =" + " " + Number);
+        for (int i = 1; i <= Number; i++)
+        {
+            if (Number % i == 0)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadLine();
+
+
+    }
+}
